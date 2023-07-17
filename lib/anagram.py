c45d5356d4d1e8d16d86bab758dfdbc695702964
Jimmy Li <jmyli562@gmail.com>
2023-07-17 15:00:05 -0700

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        matchList = []
+        for word in list:
+            if (sorted(word)) == sorted(self.word):
+                matchList.append(word)
+
+        return matchList
